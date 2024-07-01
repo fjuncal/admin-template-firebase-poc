324fd78a4312e0e6f4055f74117cae76e5b05601
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-01 19:15:24 -0300

    acertando a configuracao para pegar ao api key correta

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,9 +3,9 @@ import 'firebase/auth'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     })
 }
 
