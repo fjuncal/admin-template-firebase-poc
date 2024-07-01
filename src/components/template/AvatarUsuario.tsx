c6539ae3b3a73a2ce39145f5ae334b63c35f5e7d
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-01 19:24:45 -0300

    Criando propriedade no componente de avatar usuario caso queria que ele seja personalizado

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -1,14 +1,17 @@
 import useAuth from "@/data/hook/useAuth";
 import Link from "next/link";
 
-export default function AvatarUsuario() {
+interface AvatarUsuarioProps {
+  className?: string;
+}
+export default function AvatarUsuario(props: AvatarUsuarioProps) {
   const { usuario } = useAuth();
   return (
     <Link href={"/perfil"}>
       <img
         src={usuario?.imagemUrl ?? "/images/avatar.svg"}
         alt="Avatar do usuário"
-        className="h-10 w-10 rounded-full cursor-pointer"
+        className={`h-10 w-10 rounded-full cursor-pointer ${props.className}`}
       />
     </Link>
   );
--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -13,9 +13,9 @@ export default function Cabecalho(props: CabecalhoProps) {
   return (
     <div className={`flex`}>
       <Titulo titulo={props.titulo} subtitulo={props.subtitulo} />
-      <div className={`flex flex-grow justify-end`}>
+      <div className={`flex flex-grow justify-end items-center`}>
         <BotaoAlternarTema tema={tema!} alternarTema={alternarTema!} />
-        <AvatarUsuario />
+        <AvatarUsuario className="ml-3" />
       </div>
     </div>
   );
