2cc00a7a7276c4f613bec72971fd90616bc94c82
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-01 17:57:57 -0300

    Criando classe de modelo de Usuario e Criando o contexto de autenticação

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,1 +1,42 @@
+import { createContext, useState } from "react";
 import firebase from "../../firebase/config";
+import Usuario from "@/model/Usuario";
+
+interface AuthContextProps {
+  usuario?: Usuario;
+  loginGoogle?: () => Promise<void>;
+}
+const AuthContext = createContext<AuthContextProps>({});
+
+async function usuarioNormalizado(
+  usuarioFireBase: firebase.User
+): Promise<Usuario> {
+  const token = await usuarioFireBase.getIdToken();
+  return {
+    uid: usuarioFireBase.uid,
+    nome: usuarioFireBase.displayName!,
+    email: usuarioFireBase.email!,
+    token: token,
+    provedor: usuarioFireBase.providerData[0]?.providerId!,
+    imagemUrl: usuarioFireBase.photoURL!,
+  };
+}
+
+export function AuthProvider(props: any) {
+  const [usuario, setUsuario] = useState<Usuario>();
+
+  async function loginGoogle() {}
+
+  return (
+    <AuthContext.Provider
+      value={{
+        usuario,
+        loginGoogle,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
